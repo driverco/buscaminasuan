8712b442beb3412ec945c1f04b54be6c1d602162
Drincon28 <drincon28@uan.edu.co>
2019-04-07 19:36:23 -0500

    Actualizacion de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { userActions } from '../_actions';
 
-class Login extends React.Component {
+class LoginPage extends React.Component {
     constructor(props) {
         super(props);
 
@@ -12,8 +12,8 @@ class Login extends React.Component {
         this.props.dispatch(userActions.logout());
 
         this.state = {
-            Usuario: '',
-            Contrasena: '',
+            username: '',
+            password: '',
             submitted: false
         };
 
@@ -30,32 +30,32 @@ class Login extends React.Component {
         e.preventDefault();
 
         this.setState({ submitted: true });
-        const { Usuario, Contrasena } = this.state;
+        const { username, password } = this.state;
         const { dispatch } = this.props;
-        if (Usuario && Contrasena) {
-            dispatch(userActions.login(Usuario, Contrasena));
+        if (username && password) {
+            dispatch(userActions.login(username, password));
         }
     }
 
     render() {
         const { loggingIn } = this.props;
-        const { Usuario, Contrasena, submitted } = this.state;
+        const { username, password, submitted } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !Usuario ? ' has-error' : '')}>
-                        <label htmlFor="Usuario">Usuario</label>
-                        <input type="text" className="form-control" name="Usuario" value={Usuario} onChange={this.handleChange} />
-                        {submitted && !Usuario &&
-                            <div className="help-block">Usuario es requiredo</div>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
                         }
                     </div>
-                    <div className={'form-group' + (submitted && !Contrasena ? ' has-error' : '')}>
-                        <label htmlFor="Contrasena">Contraseña</label>
-                        <input type="Contrasena" className="form-control" name="Contrasena" value={Contrasena} onChange={this.handleChange} />
-                        {submitted && !Contrasena &&
-                            <div className="help-block">Contrasena es requireda</div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
                         }
                     </div>
                     <div className="form-group">
@@ -63,7 +63,7 @@ class Login extends React.Component {
                         {loggingIn &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
-                        <Link to="/registrar" className="btn btn-link">Registrar</Link>
+                        <Link to="/register" className="btn btn-link">Register</Link>
                     </div>
                 </form>
             </div>
@@ -78,6 +78,5 @@ function mapStateToProps(state) {
     };
 }
 
-const connectedLogin = connect(mapStateToProps)(Login);
-export { connectedLogin as Login };
-export default Login;
+const connectedLoginPage = connect(mapStateToProps)(LoginPage);
+export { connectedLoginPage as LoginPage };
