82aea7e2885bf795ddd452c4882f44d08a138571
Wilmer Rodriguez <driverco@gmail.com>
2019-04-17 08:34:18 -0500

    complejidad de la contraseña terminada

---
+++ client/src/components/PasswordStrengthMeter.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import zxcvbn from 'zxcvbn';
+import './PasswordStrengthMeter.css';
+
+class PasswordStrengthMeter extends Component {
+    
+  render() {
+    const testedResult = zxcvbn(this.props.password);
+    return (
+        <div className="password-strength-meter">
+            <progress
+                className={`password-strength-meter-progress strength-${this.createPasswordLabel(testedResult)}`}
+                value={testedResult.score}
+                max="4"
+            />    
+            <br />
+            <label className="password-strength-meter-label">
+            {this.props.password && (<>
+              <strong>Password strength:</strong> {this.createPasswordLabel(testedResult)}
+              </>)}
+         
+            </label>
+        </div>
+    );
+  }
+  createPasswordLabel = (result) => {
+    switch (result.score) {
+      case 0:
+        return 'Muy Débil';
+      case 1:
+        return 'Débil';
+      case 2:
+        return 'Fácil';
+      case 3:
+        return 'Buena';
+      case 4:
+        return 'Fuerte';
+      default:
+        return 'Muy Débil';
+    }
+  }
+}
+
+export default PasswordStrengthMeter;
